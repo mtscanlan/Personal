20fb38476f2539576e7ff2bea80af172ba471414
Matthew Scanlan <mscanlan@igloosoftware.com>
2015-07-02 11:44:57 -0400

    QuadTree - Code challenge for 2015/07/02.

---
+++ Igloo Code Kata/4tree/4tree/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4tree
+{
+    class Program
+    {
+        /*
+            Given the the following Quadtrees, write a method that compares 2 Quadtrees with n depth and outputs
+            another Quadtree where any value that matches, stays the same in the output and any numbers that don't, are set to -1.
+         */
+
+        static void Main(string[] args)
+        {
+            // Creation
+            QuadTree qt1 = new QuadTree();
+            qt1.Nodes[0] = new QuadTree(new QuadTree(5), new QuadTree(7), new QuadTree(2), new QuadTree(4));
+            qt1.Nodes[1] = new QuadTree(5);
+            qt1.Nodes[2] = new QuadTree(3);
+            qt1.Nodes[3] = new QuadTree(new QuadTree(6), new QuadTree(new QuadTree(5), new QuadTree(6), new QuadTree(12), new QuadTree(9)), new QuadTree(0), new QuadTree(3));
+
+            QuadTree qt2 = new QuadTree();
+            qt2.Nodes[0] = new QuadTree(new QuadTree(6), new QuadTree(7), new QuadTree(4), new QuadTree(4));
+            qt2.Nodes[1] = new QuadTree(7);
+            qt2.Nodes[2] = new QuadTree(4);
+            qt2.Nodes[3] = new QuadTree(6);
+
+            //Intersect the two quadtrees.
+            QuadTree output = Intersect(qt1, qt2);
+
+            // Testing
+            QuadTree expected = new QuadTree();
+            expected.Nodes[0] = new QuadTree(new QuadTree(-1), new QuadTree(7), new QuadTree(-1), new QuadTree(4));
+            expected.Nodes[1] = new QuadTree(-1);
+            expected.Nodes[2] = new QuadTree(-1);
+            expected.Nodes[3] = new QuadTree(new QuadTree(6), new QuadTree(new QuadTree(-1), new QuadTree(6), new QuadTree(-1), new QuadTree(-1)), new QuadTree(-1), new QuadTree(-1));
+            Debug.Assert(output.Equals(expected));
+
+            //Done
+            Console.ReadKey();
+        }
+
+        public static QuadTree Intersect(QuadTree quadOne, QuadTree quadTwo)
+        {
+            QuadTree quadReturn = new QuadTree();
+            
+            // Compare the values
+            quadReturn.Value = quadOne.Value == quadTwo.Value ? quadOne.Value : -1;
+            
+            // Trickle down the if one tree has nodes and the other doesn't, stops when both quadOne and quadTwo have no children.
+            if (quadOne.HasChildren && !quadTwo.HasChildren)
+            {
+                quadTwo = new QuadTree(new QuadTree(quadTwo.Value), new QuadTree(quadTwo.Value), new QuadTree(quadTwo.Value), new QuadTree(quadTwo.Value));
+            }
+            else if (quadTwo.HasChildren && !quadOne.HasChildren)
+            {
+                quadOne = new QuadTree(new QuadTree(quadOne.Value), new QuadTree(quadOne.Value), new QuadTree(quadOne.Value), new QuadTree(quadOne.Value));
+            }
+
+            // if both have children, recursively call Intersect on each node in the quadtree.
+            if (quadOne.HasChildren && quadTwo.HasChildren)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    quadReturn.Nodes[i] = Intersect(quadOne.Nodes[i], quadTwo.Nodes[i]);
+                }
+            }
+            else
+            {
+                quadReturn.HasChildren = false;
+            }
+
+            return quadReturn;
+        }
+
+        public bool Equals(QuadTree quadOne, QuadTree quadTwo)
+        {
+            bool nodesMatch = false;
+
+            if (quadOne.Value == quadTwo.Value && quadOne.HasChildren == quadTwo.HasChildren)
+            {
+                nodesMatch = true;
+                for (int i = 0; i < 4 && nodesMatch && quadOne.HasChildren; i++)
+                {
+                    nodesMatch &= quadOne.Nodes[i].Equals(quadTwo.Nodes[i]);
+                }
+            }
+
+            return nodesMatch;
+        }
+    }
+}
---
+++ Igloo Code Kata/4tree/4tree/QuadTree.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4tree
+{
+    public class QuadTree
+    {
+        public Int32 Value { get; set; }
+
+        public QuadTree[] Nodes { get; set; }
+        public bool HasChildren { get; set; }
+
+        public QuadTree()
+        {
+            Value = -1;
+            HasChildren = true;
+            Nodes = new QuadTree[4];
+        }
+
+        public QuadTree(int value)
+        {
+            HasChildren = false;
+            Nodes = new QuadTree[4];
+            Value = value;
+        }
+
+        public QuadTree(QuadTree node1, QuadTree node2, QuadTree node3, QuadTree node4)
+        {
+            Value = -1;
+            HasChildren = true;
+            Nodes = new QuadTree[4];
+            Nodes[0] = node1;
+            Nodes[1] = node2;
+            Nodes[2] = node3;
+            Nodes[3] = node4;
+        }
+
+        public override bool Equals(object obj)
+        {
+            bool nodesMatch = false;
+            if (obj is QuadTree)
+            {
+                var castedObj = obj as QuadTree;
+                if (Value == castedObj.Value && HasChildren == castedObj.HasChildren)
+                {
+                    nodesMatch = true;
+                    for (int i = 0; i < 4 && nodesMatch && HasChildren; i++)
+                    {
+                        nodesMatch &= Nodes[i].Equals(castedObj.Nodes[i]);
+                    }
+                }
+            }
+
+            return nodesMatch;
+        }
+    }
+}
