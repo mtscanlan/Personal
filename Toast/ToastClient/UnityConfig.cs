94f64daeb6222b571db7c5fa4492926ace148daf
Matthew Scanlan <mscanlan@igloosoftware.com>
2016-12-07 13:16:58 -0500

    Toast Messages - Initial commit.

---
+++ Toast/ToastClient/UnityConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Practices.Unity;
+
+namespace ToastClient
+{
+    public class UnityConfig
+    {
+        private static readonly Lazy<IUnityContainer> _container = new Lazy<IUnityContainer>(() =>
+        {
+            var container = new UnityContainer();
+            RegisterTypes(container);
+            return container;
+        });
+        
+        public static IUnityContainer GetConfiguredContainer()
+        {
+            return _container.Value;
+        }
+        
+        public static void RegisterTypes(IUnityContainer container)
+        {
+
+        }
+    }
+}
---
+++ Toast/ToastUtil/ArgumentHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ToastUtil
+{
+    public static class ArgumentHelper
+    {
+        public static T ArgumentNullCheck<T>(this T value, string paramName)
+        {
+            if (value == null)
+            {
+                throw new ArgumentException($"{nameof(paramName)} cannot be null.");
+            }
+
+            return value;
+        }
+    }
+}
