96819108611290f0987bf97b322d2dc2a0b9a09a
CROSSCHASM\mscanlan <mscanlan@fleetcarma.com>
2017-04-19 09:50:05 -0400

    Service Fabric Actors - Initial commit.

---
+++ Scratch Work/ServiceFabric-Actors/Actor1/INotificationActor.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Actors;
+
+namespace Actor1
+{
+    public interface INotificationActor : IActor
+    {
+        Task DoStuff();
+    }
+}
